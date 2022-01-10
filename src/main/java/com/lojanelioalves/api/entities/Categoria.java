44008750315ccaa41236e6b60f879366f0241ddd
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2022-01-10 14:59:11 -0300

    Teste da classe de entidade - Categoria

---
+++ src/main/java/com/lojanelioalves/api/entities/Categoria.java
@@ -0,0 +1,50 @@
+package com.lojanelioalves.api.entities;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return getId() != null ? getId().equals(categoria.getId()) : categoria.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
--- src/main/java/com/lojanelioalves/api/resources/CategoriaResource.java
+++ src/main/java/com/lojanelioalves/api/resources/CategoriaResource.java
@@ -1,17 +1,27 @@
 package com.lojanelioalves.api.resources;
 
+import com.lojanelioalves.api.entities.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando!";
+    public List<Categoria> listar(){
+        Categoria categoria1 = new Categoria(1, "Informatica");
+        Categoria categoria2 = new Categoria(2, "Escritorio");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria2);
+        lista.add(categoria2);
+
+        return lista;
     }
 
 
