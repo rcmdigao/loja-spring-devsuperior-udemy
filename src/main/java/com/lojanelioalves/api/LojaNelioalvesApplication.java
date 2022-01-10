304b8258ae85c67a17bf4ee7fac5c2cd78f9b489
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2022-01-10 11:04:52 -0300

    Projeto iniciado

---
+++ src/main/java/com/lojanelioalves/api/LojaNelioalvesApplication.java
@@ -0,0 +1,13 @@
+package com.lojanelioalves.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaNelioalvesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaNelioalvesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lojanelioalves/api/LojaNelioalvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojanelioalves.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaNelioalvesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
